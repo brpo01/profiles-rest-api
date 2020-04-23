8747e370097c42ca275dbd90868b86eab09d2d0f
Praise <opraise139@gmail.com>
2020-04-23 13:17:07 +0100

    added post,put,patch,delete fxns to APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,10 +4,14 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 # Create your views here.
 
 class HelloApiView(APIView):
+    
+    serializer_class = serializers.HelloSerializer
     
     def get(self, request, format=None):
 
@@ -21,4 +25,39 @@ class HelloApiView(APIView):
         return Response({
             'message':'Hello!',
             'an apiview': an_apiview,
+        })
+        
+    def post(self, request):
+        
+        serializer = serializers.HelloSerializer(data=request.data)
+        
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            
+            return Response({
+                'message':message,
+            })
+            
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def put(self, request, pk=None):
+        """ Handles updating an object """
+        return Response({
+            'method':'put'
+        })
+        
+    def patch(self, request, pk=None):
+        """ only updates fields provided in request """
+        
+        return Response({
+            'method':'patch'
+        })
+        
+    def delete(self, request, pk=None):
+        """ deletes an object  """
+        
+        return Response({
+            'method':'delete'
         })
