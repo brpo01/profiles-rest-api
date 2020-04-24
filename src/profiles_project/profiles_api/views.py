957e9c678bdcdb0b1931482fcd03042847c9c691
Praise <opraise139@gmail.com>
2020-04-24 13:31:35 +0100

    Added search fxn to UserProfileViewSet

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -10,6 +10,7 @@ from rest_framework import viewsets
 from . import models
 from . import permissions
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 # Create your views here.
 
 class HelloApiView(APIView):
@@ -135,4 +136,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     
     permission_classes = (permissions.UpdateOwnProfile, )
     
+    filter_backends = (filters.SearchFilter, )
+    
+    search_fields = ('name', 'email', )
     
